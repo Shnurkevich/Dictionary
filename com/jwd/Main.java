180c39bf6712650454e5f0ada80bed4b48b3f9e4
Aliaksandr Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-08-11 14:38:40 +0300

    Add files via upload

---
+++ com/jwd/Main.java
@@ -0,0 +1,8 @@
+import com.jwd.console.DictionaryConsoleApp;
+
+public class Main {
+    public static void main(String[] args) {
+        DictionaryConsoleApp app = new DictionaryConsoleApp();
+        app.start();
+    }
+}
